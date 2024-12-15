178842b66350256f8e02a6329e2856b486f7a5e5
Kamaruzzaman Zuel <zuel@faqi.com.au>
2024-12-15 15:47:10 +0600

    Props extented

--- source_code_1/src/routes/+page.svelte
+++ source_code_1/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-	import Header from "./Header.svelte";
+	import Header from './Header.svelte';
+	let age = 10;
+	let statedName = $state('Scott');
 </script>
 
-<Header name="Scott" />
+<Header name="Scott" {age} {statedName} />
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- source_code_1/src/routes/Header.svelte
+++ source_code_1/src/routes/Header.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	type TypeProps = {
 		name: string;
+		age: number;
+		statedName?: string;
 	};
 
-	let { name }: TypeProps = $props();
+	let { name, age, statedName }: TypeProps = $props();
 </script>
 
-<h1>Hello from {name}</h1>
+<h1>Hello from {name} with age {age} and stated name is {statedName}</h1>
 
 <style>
 	h1 {
