bf9bf2192018f5f7a3f25573341f95b7bdcc9dda
Kamaruzzaman Zuel <zuel@faqi.com.au>
2024-12-15 16:35:06 +0600

    Derived state

--- source_code_1/src/routes/+page.svelte
+++ source_code_1/src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import Header from './Header.svelte';
 	let age = 10;
-	let statedName = $state('Scott');
 
+	let statedName = $state('Scott');
 	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+	let fisrtName = $state('Scott');
+	let lastName = $state('Smith');
+
+	let derivedName = $derived(`${fisrtName} ${lastName}`);
 
 	const toggleStatus = () => {
 		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
@@ -14,12 +19,23 @@
 	};
 </script>
 
-<Header name="Scott" {age} {statedName} />
+<Header name="Scott" {age} {statedName} {derivedName} />
 
 <input type="text" bind:value={statedName} />
 <!-- <button on:click={toggleStatus}>{status}</button> -->
 <button onclick={toggleStatus}>{status}</button>
 <button {onclick}>{status}</button>
+
+<!-- #region derived state -->
+<label aria-label="First Name" for="firstName">First Name</label>
+<input type="text" bind:value={fisrtName} />
+
+<label aria-label="Last Name" for="lastName">Last Name</label>
+<input type="text" bind:value={lastName} />
+
+<p>{derivedName}</p>
+
+<!-- #endregion -->
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- source_code_1/src/routes/Header.svelte
+++ source_code_1/src/routes/Header.svelte
@@ -2,13 +2,15 @@
 	type TypeProps = {
 		name: string;
 		age: number;
+		derivedName: string;
 		statedName?: string;
 	};
 
-	let { name, age, statedName }: TypeProps = $props();
+	let { name, age, statedName, derivedName }: TypeProps = $props();
 </script>
 
 <h1>Hello from {name} with age {age} and stated name is {statedName?.replaceAll('t', 'x')}</h1>
+<p>full name is {derivedName}</p>
 
 <style>
 	h1 {
