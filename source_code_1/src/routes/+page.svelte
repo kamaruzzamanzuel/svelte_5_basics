e32d86b756763e0ffd8a1606e339747c8cdae939
Kamaruzzaman Zuel <zuel@faqi.com.au>
2024-12-15 18:01:36 +0600

    Conditionals and logic

--- source_code_1/src/routes/+page.svelte
+++ source_code_1/src/routes/+page.svelte
@@ -17,25 +17,77 @@
 	const onclick = () => {
 		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
 	};
+
+	// #region form state
+	let formState = $state({
+		name: '',
+		birthdate: '',
+		step: 0,
+		error: ''
+	});
+	// #endregion
 </script>
 
-<Header name="Scott" {age} {statedName} {derivedName} />
+<main>
+	<Header name="Scott" {age} {statedName} {derivedName} />
 
-<input type="text" bind:value={statedName} />
-<!-- <button on:click={toggleStatus}>{status}</button> -->
-<button onclick={toggleStatus}>{status}</button>
-<button {onclick}>{status}</button>
+	<input type="text" bind:value={statedName} />
+	<!-- <button on:click={toggleStatus}>{status}</button> -->
+	<button onclick={toggleStatus}>{status}</button>
+	<button {onclick}>{status}</button>
 
-<!-- #region derived state -->
-<label aria-label="First Name" for="firstName">First Name</label>
-<input type="text" bind:value={fisrtName} />
+	<!-- #region derived state -->
+	<label aria-label="First Name" for="firstName">First Name</label>
+	<input type="text" bind:value={fisrtName} />
 
-<label aria-label="Last Name" for="lastName">Last Name</label>
-<input type="text" bind:value={lastName} />
+	<label aria-label="Last Name" for="lastName">Last Name</label>
+	<input type="text" bind:value={lastName} />
 
-<p>{derivedName}</p>
+	<p>{derivedName}</p>
 
-<!-- #endregion -->
+	<!-- #endregion -->
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+	<!-- #region form -->
+	<h1>Form</h1>
+	<p>Step : {formState.step}</p>
+
+	{#if formState.error}
+		<p class="error">{formState.error}</p>
+	{/if}
+
+	{#if formState.step === 0}
+		<div>
+			<label for="name">Name</label>
+			<input type="text" bind:value={formState.name} />
+		</div>
+
+		<button
+			onclick={() => {
+				if (formState.name) {
+					formState.step += 1;
+					formState.error = '';
+				} else {
+					formState.error = 'Name is required';
+				}
+			}}>Next</button
+		>
+
+		{:else if formState.step === 1}
+		
+		<div>
+			<label for="birthdate">Birthdate</label>
+			<input type="date" bind:value={formState.birthdate} />
+		</div>
+
+		<button
+			onclick={() => {
+				if (formState.birthdate) {
+					formState.step += 1;
+					formState.error = '';
+				} else {
+					formState.error = 'Birthdate is required';
+				}
+			}}>Next</button
+		>
+	{/if}
+</main>
